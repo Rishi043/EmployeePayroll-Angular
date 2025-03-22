55462d683c416f1d33ed2ce6f8a34d0f4d7044f5
Rishi Thakur <rishithakur9131@gmail.com>
2025-03-22 14:18:20 +0530

    [Rishi] Add. update details feature in EmployeePayroll

--- Employeepayroll/src/app/services/employee-services.service.ts
+++ Employeepayroll/src/app/services/employee-services.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeServicesService {
+  private apiUrl = 'http://localhost:8080/employees'; // Backend API URL
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  // Fetch all employees
+  getEmployees(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  // Add a new employee
+  addEmployee(employee: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, employee);
+  }
+
+  //Get a single employee by ID
+  getEmployeeById(id: number): Observable<any>{
+    return this.http.get<any>(`${this.apiUrl}/${id}`);
+  }
+
+  //Update an existing employee
+  updateEmployee(id:number, employeeData: any): Observable<any>{
+    return this.http.put<any>(`${this.apiUrl}/${id}`, employeeData);
+  }
 }
+
