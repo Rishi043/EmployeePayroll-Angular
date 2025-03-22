6df5040587ff606b4cf3b1b6cc91efcc6262bc03
Rishi Thakur <rishithakur9131@gmail.com>
2025-03-22 13:43:50 +0530

    [Rishi] Add. add data feature in EmployeePayroll

--- Employeepayroll/src/app/add-employee/add-employee.component.ts
+++ Employeepayroll/src/app/add-employee/add-employee.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { EmployeeServicesService } from '../services/employee-services.service';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-add-employee',
-  imports: [],
+  standalone: true,
   templateUrl: './add-employee.component.html',
-  styleUrl: './add-employee.component.css'
+  styleUrls: ['./add-employee.component.css'],
+  imports: [FormsModule] // Required for ngModel
 })
 export class AddEmployeeComponent {
 
+  employee = {
+    name: '',
+    gender: '',
+    department: '',
+    salary: '',
+    startDate: '',
+    profile_pic:'',
+    note:''
+  };
+
+  constructor(
+    private employeeService: EmployeeServicesService,
+    private router: Router,
+    private http: HttpClient
+  ) {}
+
+  addEmployee() {
+    console.log("adding Employee------",this.employee);
+    this.http.post("http://localhost:8080/employees",this.employee).subscribe({complete:() => {this.router.navigateByUrl('/') }, error:() =>{alert("Something went wrong!!")}})
+  }
 }
